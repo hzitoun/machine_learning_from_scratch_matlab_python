6b85da1de7cd65214d0c571e60a6db40f308f434
hzitoun <zitoun.hamed@gmail.com>
2018-02-04 17:05:32 +0100

    adding tuto 6

---
+++ src/week_2/tuto_6_vectorization.m
@@ -0,0 +1,6 @@
+%Always use vectorization instead of a for loop
+%to compute gradient descent for linear regression
+%for j
+% wj = wj - alpha * 1/m * SUMi (h(xi) - yi) * xji
+%instead do
+% W = W - alpha * 1/M * SUMi (h(xi) - yi) * Xi
