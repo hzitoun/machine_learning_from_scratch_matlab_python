66da67bb3796429b5c065da4d3e934999dc82e57
hzitoun <zitoun.hamed@gmail.com>
2018-03-18 22:29:56 +0100

    clustering with Kmeans + PCA + image compression

---
+++ algorithms_in_matlab/week_8/ex7/computeCentroids.m
@@ -0,0 +1,37 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+n = size(X, 2);
+
+centroids = zeros(K, n);
+
+[~, ~, ii] = unique(idx);
+for i=1:n
+centroids(:, i) = accumarray(ii, X(:,i),[], @mean);
+end
+
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
---
+++ algorithms_in_matlab/week_8/ex7/findClosestCentroids.m
@@ -0,0 +1,23 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+m = size(X, 1);
+idx = zeros(m, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+for i=1:m
+   V = centroids - X(i, :);
+   D = vecnorm(V, 2, 2) .^ 2;
+   [~, idx(i)] = min(D);
+end
+
+end
+
