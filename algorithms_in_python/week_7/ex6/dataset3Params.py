aee3741dae5b3c18cff349fc1a0c100622245dc7
hzitoun <zitoun.hamed@gmail.com>
2018-04-28 22:34:04 +0200

    SupportVectorMachine Model using Linear and RBF kernels

--- algorithms_in_python/week_7/ex6/dataset3Params.py
+++ algorithms_in_python/week_7/ex6/dataset3Params.py
@@ -5,12 +5,12 @@ from svmModel import SVMModel
 
 def dataset3Params(X, y, Xval, yval):
     """
-    %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
-    %where you select the optimal (C, sigma) learning parameters to use for SVM
-    %with RBF kernel
-    %   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
-    %   sigma. You should complete this function to return the optimal C and
-    %   sigma based on a cross-validation set.
+    Returns your choice of C and sigma for Part 3 of the exercise
+    where you select the optimal (C, sigma) learning parameters to use for SVM
+    with RBF kernel
+       [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+       sigma. You should complete this function to return the optimal C and
+       sigma based on a cross-validation set.
     """
 
     # You need to return the following variables correctly.
